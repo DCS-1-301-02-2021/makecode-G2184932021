46e128fc1af9027ba7af1fcbf7771e79c164646d
G2184932021 <86589973+G2184932021@users.noreply.github.com>
2021-07-30 22:57:53 +0900

    ゲームを完成させた

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "teki":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f . . . . . . . f . . . 
+. . . f . f . . . . . f . f . . 
+. . f . . . f . . . f . . . f . 
+. f . . . . . f . f . . . . . f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
